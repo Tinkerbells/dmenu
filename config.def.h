b6a76995d344209ce99a297f30015964196013a5
Unter Mensch <untermenschh@protonmail.com>
2022-11-13 14:00:48 +0300

    update config

--- config.def.h
+++ config.def.h
@@ -2,12 +2,16 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static char font[] = "JetBrainsMono NF:size=12";
-static char *fonts[] = {
-  font
+static char font[] = "monospace:size=10";
+static const char *fonts[] = {
+	font,
+	"monospace:size=10",
 };
+
 static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
 static char normfgcolor[] = "#bbbbbb";
 static char normbgcolor[] = "#222222";
 static char selfgcolor[]  = "#eeeeee";
